b5291487d2a240d99a1712c31b30e68934e5c9b8
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2024-12-01 23:38:59 +0800

    feat: add gsap

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,38 @@
 "use client";
 
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
 import { LoginForm } from "@/components/login-form";
 
 export default function Page() {
+  const imageRef = useRef(null);
+  const formRef = useRef(null);
+
+  useEffect(() => {
+    const tl = gsap.timeline({ defaults: { duration: 1, ease: "power3.out" } });
+
+    tl.from(imageRef.current, { x: -200, opacity: 0 }).from(
+      formRef.current,
+      { x: 200, opacity: 0 },
+      "-=0.5"
+    );
+  }, []);
+
   return (
     <div className="relative flex h-screen w-full items-center justify-center px-4 lg:px-0">
       {/* Side image for large devices, background image for smaller devices */}
       <div
+        ref={imageRef}
         className="absolute inset-0 bg-cover bg-center lg:relative lg:w-1/2 lg:h-full lg:block"
         style={{
           backgroundImage: 'url("/login.png")',
         }}
       ></div>
 
-      <div className="z-10 flex h-full w-full max-w-md items-center justify-center  p-6 shadow-lg lg:ml-auto lg:max-h-full lg:w-1/2 lg:max-w-none">
+      <div
+        ref={formRef}
+        className="z-10 flex h-full w-full max-w-md items-center justify-center p-6 shadow-lg lg:ml-auto lg:max-h-full lg:w-1/2 lg:max-w-none"
+      >
         <LoginForm />
       </div>
     </div>
