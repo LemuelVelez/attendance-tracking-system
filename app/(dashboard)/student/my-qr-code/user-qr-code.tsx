328927be2e85706b5d9a8c06a105bb959d0533dd
lemuelvelez <pluklem07@gmail.com>
2025-02-05 15:51:43 +0800

    feat: update the color of loader2 in my qr code student dashboard

--- app/(dashboard)/student/my-qr-code/user-qr-code.tsx
+++ app/(dashboard)/student/my-qr-code/user-qr-code.tsx
@@ -63,7 +63,7 @@ export default function UserQR() {
   if (loading) {
     return (
       <div className="flex justify-center items-center h-screen">
-        <Loader2 className="h-8 w-8 animate-spin" />
+        <Loader2 className="h-8 w-8 animate-spin text-primary" />
       </div>
     );
   }
